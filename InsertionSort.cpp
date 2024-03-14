8346eac37bb67c556ab1d894d3b25365922dfa76
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-14 15:17:31 +0700

    selesai

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,6 +4,30 @@ using namespace std;
 int arr[20];
 int n;
 
+void input(){
+    while (true)
+    {
+        cout <<"masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1 ) << ": ";
+        cin >> arr[i];
+    }
+    
+} 
+
 void insertionSort() {
     int i, j, temp;
 
@@ -20,4 +44,26 @@ void insertionSort() {
 
         arr[j + 1] = temp; //step 5
     }
+}
+
+void display (){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for( int j = 0; j < n; j++){
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n-1  << endl;
+    cout << endl;  
+}
+
+int main()
+{
+    input();
+    insertionSort();
+    display();
+
+    system("pauase");
+    return 0;
 }
