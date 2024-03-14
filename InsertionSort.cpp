7015b2429b31a055fd6f28797a5b62f5e240c7a0
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-14 15:15:50 +0700

    step 4

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -11,5 +11,11 @@ void insertionSort() {
         temp = arr[i]; //step 2
 
         j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j+1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
     }
 }
